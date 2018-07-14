9e10eb7c7892d07bc879775babd1543674020a40
Quentin CAUDRON <quentincaudron@gmail.com>
2018-07-14 10:02:05 -0700

    First pass at a simplified Supernova

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name="supernova",
+    version="0.1.0",
+    description="Blows away all that is unnecessary.",
+    url="http://github.com/QCaudron/supernova",
+    author="Quentin CAUDRON",
+    author_email="quentincaudron@gmail.com",
+    license="MIT",
+    packages=["supernova"],
+    zip_safe=False,
+    install_requires=[
+        "numpy>=1.13",
+        "pandas>=0.22",
+        "scikit-learn>=0.19",
+        "xgboost>0.5"
+    ]
+)
---
+++ supernova/__init__.py
@@ -0,0 +1,1 @@
+from supernova.core import Supernova
